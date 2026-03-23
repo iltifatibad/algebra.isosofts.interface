bd7ffb5dbdd48fa74aa165f4797e995b61204487
iltifatibad <iltifatibad@gmail.com>
2026-03-23 01:12:28 +0000

    Training Bug Fix

--- components/trprofile.jsx
+++ components/trprofile.jsx
@@ -1308,8 +1308,8 @@ const archiveData = (id) => {
                 className="w-full px-3.5 py-2.5 bg-gray-50 border border-gray-200 rounded-xl text-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent focus:bg-white transition-all"
               >
                 <option value="">Select</option>
-                <option value="0">Valid</option>
-                <option value="1">Not Valid</option>
+                <option value={0}>Valid</option>
+                <option value={1}>Not Valid</option>
               </select>
             </div>
 
@@ -1322,8 +1322,8 @@ const archiveData = (id) => {
                 className="w-full px-3.5 py-2.5 bg-gray-50 border border-gray-200 rounded-xl text-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent focus:bg-white transition-all"
               >
                 <option value="">Select</option>
-                <option value="0">No</option>
-                <option value="1">Yes</option>
+                <option value={0}>No</option>
+                <option value={1}>Yes</option>
               </select>
             </div>
           </div>
