26b3c78f0ea262abe424c85f56b6df4e0295d2e2
iltifatibad <iltifatibad@gmail.com>
2026-04-04 22:32:33 +0000

    Litte Bug Fix

--- components/eiprofile.jsx
+++ components/eiprofile.jsx
@@ -1277,7 +1277,7 @@ const archiveData = (id) => {
                       className="w-full px-3.5 py-2.5 bg-gray-50 border border-gray-200 rounded-xl text-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent focus:bg-white transition-all"
                     >
                       <option value="">Select</option>
-                      {dropdownData?.type?.map((item) => (
+                      {dropdownData?.equipmentType?.map((item) => (
                         <option key={item.id} value={item.id}>
                           {item.value}
                         </option>
