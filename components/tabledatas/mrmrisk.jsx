23bed8456f79342a9f6fd57b7f91a5bc2c7dc153
iltifatibad <iltifatibad@gmail.com>
2026-04-07 22:07:48 +0000

    Final V1

--- components/tabledatas/mrmrisk.jsx
+++ components/tabledatas/mrmrisk.jsx
@@ -305,15 +305,16 @@ console.log("URL:", url); // Debug: URL'yi konsola yazdır, registerId'yi kontro
   <SoftBadge value={row.process?.value} color="bg-violet-100 text-violet-700 border border-violet-200" />
 </td>
 
+{/* Topic */}
+<td className="border border-gray-200 px-2 py-1 w-32" rowSpan={1}>
+  <SoftBadge value={row.topic?.value} color="bg-blue-100 text-blue-700 border border-blue-200" />
+</td>
+
 {/* Comment */}
 <td className="border border-gray-200 px-3 py-2 w-20" rowSpan={1}>
   <SoftBadge value={row.comment} color="bg-green-100 text-green-700 border border-green-200" />
 </td>
 
-{/* Topic */}
-<td className="border border-gray-200 px-2 py-1 w-32" rowSpan={1}>
-  <SoftBadge value={row.topic?.value} color="bg-blue-100 text-blue-700 border border-blue-200" />
-</td>
                 </tr>
 
                 {/* Ek Actions */}
@@ -388,15 +389,16 @@ console.log("URL:", url); // Debug: URL'yi konsola yazdır, registerId'yi kontro
   <SoftBadge value={row.process?.value} color="bg-violet-100 text-violet-700 border border-violet-200" />
 </td>
 
+{/* Topic */}
+<td className="border border-gray-200 px-2 py-1 w-32" rowSpan={1}>
+  <SoftBadge value={row.topic?.value} color="bg-blue-100 text-blue-700 border border-blue-200" />
+</td>
+
 {/* Comment */}
 <td className="border border-gray-200 px-3 py-2 w-20" rowSpan={1}>
   <SoftBadge value={row.comment} color="bg-green-100 text-green-700 border border-green-200" />
 </td>
 
-{/* Topic */}
-<td className="border border-gray-200 px-2 py-1 w-32" rowSpan={1}>
-  <SoftBadge value={row.topic?.value} color="bg-blue-100 text-blue-700 border border-blue-200" />
-</td>
                 </tr>
 
                 {/* Ek Actions */}
@@ -748,14 +750,14 @@ console.log("URL:", url); // Debug: URL'yi konsola yazdır, registerId'yi kontro
   <SoftBadge value={row.process?.value} color="bg-violet-100 text-violet-700 border border-violet-200" />
 </td>
 
-{/* Comment */}
-<td className="border border-gray-200 px-3 py-2 w-20" rowSpan={1}>
-  <SoftBadge value={row.comment} color="bg-green-100 text-green-700 border border-green-200" />
-</td>
-
 {/* Topic */}
 <td className="border border-gray-200 px-2 py-1 w-32" rowSpan={1}>
   <SoftBadge value={row.topic?.value} color="bg-blue-100 text-blue-700 border border-blue-200" />
+</td>
+
+{/* Comment */}
+<td className="border border-gray-200 px-3 py-2 w-20" rowSpan={1}>
+  <SoftBadge value={row.comment} color="bg-green-100 text-green-700 border border-green-200" />
 </td>
                 </tr>
               </React.Fragment>
