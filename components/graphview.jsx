127cb933080ce1361936bb966b9878e6e6b65266
iltifatibad <iltifatibad@gmail.com>
2026-05-30 09:58:15 +0000

    Print Status Bar

--- components/graphview.jsx
+++ components/graphview.jsx
@@ -232,6 +232,10 @@ export default function GraphView({ onClose }) {
         #__graphview_print_overlay__ * {
           overflow: visible !important;
           max-height: none !important;
+          /* Force browsers to print background colors & images */
+          -webkit-print-color-adjust: exact !important;
+          print-color-adjust: exact !important;
+          color-adjust: exact !important;
         }
       }
     `;
